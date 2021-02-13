fb909fdd5e76077b77c21e846751570e573d96d6
victorchilari <victor1chilari1@gmail.com>
2021-02-13 11:02:05 +0200

    incoder/ myszkowski

---
+++ ciphers/transposition.js
@@ -0,0 +1,63 @@
+const excludedChar = [' ', ',', '.'];
+function myszkowski(text, key) {
+	const str_arr = text.split('');
+	const clear_str_arr = str_arr.filter(char => !excludedChar.includes(char));
+	const arr = [];
+
+	const stringLength = clear_str_arr.length;
+	const keyWidth = key.length;
+
+	// Repartition chars into table
+	let position = 0;
+	for (let j = 0; j < stringLength / keyWidth; j++) {
+		arr.push([]);
+		for (let i = 0; i < keyWidth; i++) {
+			const chr = clear_str_arr[position];
+			arr[j].push(chr);
+			position++;
+			if (position == stringLength) {
+				break;
+			}
+		}
+	}
+
+	//// Read data by column and create a final string
+	// Read data by column and create an object ////array sorted correct
+	let encriptedArr = [];
+	const obj = {};
+	for (let j = 0; j < keyWidth; j++) {
+		let keyIsRepeated = false;
+		const curentKey = key[j];
+		if (obj[curentKey] === undefined) {
+			obj[curentKey] = [];
+		} else {
+			keyIsRepeated = true;
+		}
+		for (let i = 0; i < arr.length; i++) {
+			const char = arr[i][j];
+			if (char !== undefined) {
+				if (keyIsRepeated) {
+					const position = i * 2 + 1;
+					obj[curentKey].splice(position, 0, char);
+				} else {
+					obj[curentKey].push(char);
+				}
+				// encriptedArr.push(char);
+				//encriptedStr.concat('', char);
+			}
+		}
+	}
+	// Parse created object
+	const sortedKeys = Object.keys(obj).sort();
+	for (const key of sortedKeys) {
+		const chrsForThisKey = obj[key].length;
+		for (let i = 0; i < chrsForThisKey; i++) {
+			const char = obj[key][i];
+			encriptedArr.push(char);
+		}
+	}
+
+	console.log(encriptedArr.join(''));
+}
+
+myszkowski('WE ARE DISCOVERED. FLEE AT ONCE', 'TOMATO');
